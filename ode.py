d50e23af490b10683d5d2497bd37be2ad473f634
Pavel Stránský <pcfyzika@pavelstransky.cz>
2021-03-30 10:47:35 +0200

    Euler 2. řádu, Runge-Kutta 4. řádu

--- ode.py
+++ ode.py
@@ -5,6 +5,28 @@ def euler_1(model, y, t, dt):
     return y1, t1
 
 
+def euler_2(model, y, t, dt):
+    """ Second-order Euler method """
+    d1 = model(y, t)
+    d2 = model(y + d1 * dt, t)
+    y1 = y + 0.5 * (d1 + d2) * dt
+    t1 = t + dt
+    return y1, t1
+
+
+def runge_kutta_4(model, y, t, dt):
+    """ Fourth-order Runge-Kutta algoritm """
+    d1 = model(y, t)
+    d2 = model(y + 0.5 * d1 * dt, t + 0.5 * dt)
+    d3 = model(y + 0.5 * d2 * dt, t + 0.5 * dt)
+    d4 = model(y + d3 * dt, t + dt)
+    
+    y1 = y + (d1 + 2 * d2 + 2 * d3 + d4) * dt / 6
+    t1 = t + dt
+    
+    return y1, t1
+
+
 def ode_solve(model, initial_condition, integrator=euler_1, dt=0.1, maxt=10):
     """ Numerical solution of a differential equation by the specified integrator.
 
