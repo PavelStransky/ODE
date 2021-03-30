509b08b5551c9b48362b7c31690e04f1947c5f6b
Pavel Stránský <pcfyzika@pavelstransky.cz>
2021-03-30 10:38:36 +0200

    Eulerova metoda 1. řádu

---
+++ ode.py
@@ -0,0 +1,27 @@
+def euler_1(model, y, t, dt):
+    y1 = y + model(y, t) * dt
+    t1 = t + dt
+    return y1, t1
+
+
+def ode_solve(model, initial_condition, integrator=euler_1, dt=0.1, maxt=10):
+    y = initial_condition                   # Initial conditions
+    ys = [y]                                # List with results
+    
+    t = 0                                   # Actual time
+    ts = [t]                                # List with times
+    
+    while t < maxt:
+        y, t = integrator(model, y, t, dt)  # Step
+        
+        ys.append(y)                        # Store position
+        ts.append(t)                        # Store time
+            
+    return ys, ts
+
+
+def relaxation(y, t):
+    return -y
+
+
+ys, ts = ode_solve(relaxation, 1)
